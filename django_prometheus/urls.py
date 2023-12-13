fb6d87503e6c53830355677e6d755424fdd4c7c6
Stuart Mainwaring <stuart.mainwaring@getstream.io>
2023-12-13 11:13:41 +0000

    removed url with no trialing slash

--- django_prometheus/urls.py
+++ django_prometheus/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from django_prometheus import exports
 
 urlpatterns = [
-    path("metrics", exports.ExportToDjangoView, name="prometheus-django-metrics"),
     path("metrics/", exports.ExportToDjangoView, name="prometheus-django-metrics"),
 ]
